5e539592487a3b4b316de24e24c8ac0e3792d734
ericremoroza <eric.remoroza@outlook.com>
2019-01-19 13:24:12 -0500

    added logo ER

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,11 +1,11 @@
 import React from "react";
-import styles from "../../styles.css"
+import "../../styles.css"
 import Google from "../Signin"
 
 const Nav = () => (
   <nav className="navbar navbar-expand-sm nav-pp navbar-dark">
       {/*Brand/logo*/}
-      <a className="navbar-brand" href="/">Logo</a>
+      <a className="navbar-brand" href="/"><img className="logoIcon" alt="Logo" src="https://img.pokemondb.net/artwork/vector/luvdisc.png"/></a>
       {/*Links*/}
       <ul className="navbar-nav">
           <li className="nav-item">
