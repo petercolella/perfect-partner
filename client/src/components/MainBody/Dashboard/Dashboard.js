967de7fb1d86780abbfa73419cb15ec2e88daf1e
TonyRome <tonyromebx@gmail.com>
2019-01-26 14:21:59 -0500

    move state to MainBody

--- client/src/components/MainBody/Dashboard/Dashboard.js
+++ client/src/components/MainBody/Dashboard/Dashboard.js
@@ -1,12 +1,19 @@
-import React,{Component} from 'react'
+import React from 'react'
 
-class Dashboard extends Component {
-    state = {};
-    render() {
-        return(
-            <div className="container-fluid">Dashboard</div>
-        )
-    }
-}
+
+const Dashboard = (props)=> (
+       <div className="container-fluid">
+           {props.users.map(user => (
+               <li key={user._id}>
+                   <p>Name: {user.name}</p>
+                   <p>Phone: {user.phone}</p>
+                   <p>Nudge Frequency: {user.nudgeFrequency}</p>
+                   <p>Partner Name: {user.partnerName}</p>
+                   <p>Anniversary Date: {user.anniversaryDate}</p>
+                   <p>Birth Date: {user.birthDate}</p>
+               </li>
+           ))}
+       </div>
+);
 
 export default Dashboard
--- client/src/components/MainBody/MainBody.js
+++ client/src/components/MainBody/MainBody.js
@@ -1,13 +1,32 @@
 import React, {Component} from 'react';
-import Google from './Signin/Google'
+import API from '../../utils/API'
+import Dashboard from './Dashboard/Dashboard'
 
 class MainBody extends Component {
-    state ={};
+    state = {
+        users: [],
+        name: "",
+        phone: "",
+        nudgeFrequency: "",
+        partnerName: "",
+        anniversaryDate: "",
+        birthDate: ""
+    };
+
+    componentDidMount() {
+        this.loadUserInfo();
+    }
+    loadUserInfo = () => {
+        API.getUsers().then(res =>
+            this.setState({ users: res.data, name: "", phone: "", nudgeFrequency: "", partnerName: "", anniversaryDate: "", birthDate: "" })
+        );
+    };
+
     render(){
         return(
-            <div className="container-fluid">
-                <Google/>
-            </div>
+            <Dashboard
+                users = {this.state.users}
+            />
         )
     }
 }
