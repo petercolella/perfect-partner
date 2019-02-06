929f1a23da72dc5cd58bf8e6a31bac37fb5bd362
petercolella <peter.joseph.colella@gmail.com>
2019-02-05 20:12:35 -0500

    changed class to className and installed react-helmet

--- client/src/components/MainBody/Signin/Google.js
+++ client/src/components/MainBody/Signin/Google.js
@@ -3,93 +3,104 @@ import { Link } from 'react-router-dom';
 import API from '../../../utils/API';
 
 const styles = {
-    marginTop: 120,
-    imgStyle: {
-        height: 96,
-        width: 'auto'
-    }
+  marginTop: 120,
+  imgStyle: {
+    height: 96,
+    width: 'auto'
+  }
 };
 
 class SignIn extends Component {
-    state = {
-        currentUser: '',
-        currentUserMessage: 'Please Sign In.',
-        imageUrl: ''
-    };
+  state = {
+    currentUser: '',
+    currentUserMessage: 'Please Sign In.',
+    imageUrl: ''
+  };
 
-    createUser(profile) {
-        API.saveUser({
-            name: profile.getName(),
-            email: profile.getEmail(),
-            imageUrl: profile.getImageUrl()
-        }).catch(err => {
-            console.log(err);
-        });
+  createUser(profile) {
+    API.saveUser({
+      name: profile.getName(),
+      email: profile.getEmail(),
+      imageUrl: profile.getImageUrl()
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+
+  onSignIn = googleUser => {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+    if (profile.getName()) {
+      this.setState({
+        currentUser: profile.getName(),
+        currentUserMessage: `Hello, ${profile.getName()}`,
+        imageUrl: profile.getImageUrl()
+      });
     }
 
-    onSignIn = googleUser => {
-        var profile = googleUser.getBasicProfile();
-        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-        console.log('Name: ' + profile.getName());
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    this.createUser(googleUser.getBasicProfile());
+  };
 
-        if (profile.getName()) {
-            this.setState({
-                currentUser: profile.getName(),
-                currentUserMessage: `Hello, ${profile.getName()}`,
-                imageUrl: profile.getImageUrl()
-            });
-        }
+  onSuccess(googleUser) {
+    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
 
-        this.createUser(googleUser.getBasicProfile());
-    };
+    // ******* NEED REDIRECT CODE TO SEND TO PAGE FOR SIGN UP *************
+  }
+  onFailure(error) {
+    console.log(error);
+  }
 
-    onSuccess(googleUser) {
-        console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+  componentDidMount() {
+    window.gapi.signin2.render('my-signin2', {
+      scope: 'profile email',
+      width: 240,
+      height: 50,
+      longtitle: true,
+      theme: 'dark',
+      onsuccess: this.onSignIn,
+      onfailure: this.onFailure
+    });
+  }
 
-        // ******* NEED REDIRECT CODE TO SEND TO PAGE FOR SIGN UP *************
-    }
-    onFailure(error) {
-        console.log(error);
-    }
-
-    componentDidMount() {
-        window.gapi.signin2.render('my-signin2', {
-            scope: 'profile email',
-            width: 240,
-            height: 50,
-            longtitle: true,
-            theme: 'dark',
-            onsuccess: this.onSignIn,
-            onfailure: this.onFailure
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <div className="container">
-                    <div className="row mx-auto">
-                        <div className="avatar-center">
-                            {this.state.imageUrl ? (
-                                <img src={this.state.imageUrl} alt="profile-img" className="logo-avatar" />
-                            ) : (
-                                <img
-                                    src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png"
-                                    alt="profile-img"
-                                    style={styles.imgStyle}
-                                />
-                            )}
-                        <p className="lead">{this.state.currentUserMessage}</p>
-                         <p class="clicktext">Please click <Link to="/phone">HERE<br/></Link> to answer a question.</p>
-                        </div>
-                    </div>
-                </div>
-                <div id="my-signin2" />
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="row mx-auto">
+            <div className="avatar-center">
+              {this.state.imageUrl ? (
+                <img
+                  src={this.state.imageUrl}
+                  alt="profile-img"
+                  className="logo-avatar"
+                />
+              ) : (
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png"
+                  alt="profile-img"
+                  style={styles.imgStyle}
+                />
+              )}
+              <p className="lead">{this.state.currentUserMessage}</p>
+              <p className="clicktext">
+                Please click{' '}
+                <Link to="/phone">
+                  HERE
+                  <br />
+                </Link>{' '}
+                to answer a question.
+              </p>
             </div>
-        );
-    }
+          </div>
+        </div>
+        <div id="my-signin2" />
+      </div>
+    );
+  }
 }
 
 export default SignIn;
