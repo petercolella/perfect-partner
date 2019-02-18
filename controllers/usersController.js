5403c50cc9177ac13707f6b144bc121f93d6336c
petercolella <peter.joseph.colella@gmail.com>
2019-02-17 21:57:27 -0500

    nudges component working; had to comment out user model nudges array

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,4 @@
-const db = require("../models");
+const db = require('../models');
 
 // Defining methods for the usersController
 module.exports = {
@@ -8,26 +8,22 @@ module.exports = {
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.User
-      .findById(req.params.id)
+    db.User.findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.User
-      .create(req.body)
+    db.User.create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.User
-      .findOneAndUpdate({ _id: req.params.id }, req.body)
+    db.User.findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.User
-      .findById({ _id: req.params.id })
+    db.User.findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
--- models/nudge.js
+++ models/nudge.js
@@ -1,13 +1,12 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const nudgeSchema = new Schema({
   name: { type: String, required: true },
-  nudgeFrequency: { type: Number, required: true, default: 5000},
-  textMessage: { type: String, required: true, default: "text body" }
+  nudgeFrequency: { type: Number, required: true, default: 5000 },
+  textMessage: { type: String, default: 'text body' }
 });
 
-
-const Nudge = mongoose.model("Nudge", nudgeSchema);
+const Nudge = mongoose.model('Nudge', nudgeSchema);
 
 module.exports = Nudge;
