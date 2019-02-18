fee6901451c79f6d4386d91a543c77c03bb478b4
petercolella <peter.joseph.colella@gmail.com>
2019-02-17 21:26:02 -0500

    birthday and anniversary updated

--- client/src/components/QuestionComponents/Birthday/index.js
+++ client/src/components/QuestionComponents/Birthday/index.js
@@ -6,26 +6,57 @@ const $ = window.$;
 
 class Birthday extends Component {
   state = {
-    users: [],
     User: {},
+    userEmail: '',
     title: 'Birthday',
     question: "What is your partner's birthday?",
     userField: '',
     nextQuestionLink: '/anniversary'
   };
 
+  initClient = function() {
+    const self = this;
+    window.gapi.load('auth2', function() {
+      window.gapi.auth2
+        .init({
+          client_id:
+            '773798651320-0da27e8d6k9mo9ldaijdlupeib1r56jq.apps.googleusercontent.com'
+        })
+        .then(
+          GoogleAuth => {
+            const currentUserEmail = GoogleAuth.currentUser
+              .get()
+              .getBasicProfile()
+              .getEmail();
+            self.setState(
+              {
+                userEmail: currentUserEmail
+              },
+              () => {
+                self.loadUserInfo();
+              }
+            );
+          },
+          err => {
+            console.log(err);
+          }
+        );
+    });
+  };
+
   componentDidMount() {
-    this.loadUserInfo();
+    this.initClient();
     $('.modal-content').css(
       'background-image',
       'url(https://s3.amazonaws.com/bucket-tony-yellowstone/birthday-history-600x319.jpg)'
     );
   }
+
   loadUserInfo = () => {
-    API.getUsers().then(res => {
-      this.setState({ users: res.data, User: res.data[res.data.length - 1] });
-      console.log(res.data[res.data.length - 1]);
-      console.log(res.data[res.data.length - 1]._id);
+    const email = this.state.userEmail;
+    API.getUserByEmail(email).then(res => {
+      const resUser = res.data.shift();
+      this.setState({ User: resUser });
     });
   };
 
