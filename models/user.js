fe18f058d94714a869fd55237bb4a239c5724650
petercolella <peter.joseph.colella@gmail.com>
2019-02-18 22:26:36 -0500

    cleaned up user model

--- models/user.js
+++ models/user.js
@@ -22,18 +22,3 @@ const userSchema = new Schema({
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
-
-// const mongoose = require('mongoose');
-// const Schema = mongoose.Schema;
-//
-// const userSchema = new Schema({
-//   name: { type: String, required: true },
-//   email: { type: String, required: true, unique: true },
-//   imageUrl: { type: String },
-//   phone: { type: String },
-//   date: { type: Date, default: Date.now }
-// });
-//
-// const User = mongoose.model('User', userSchema);
-//
-// module.exports = User;
