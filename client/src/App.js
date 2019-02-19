d8d7e33fadddf2a167818d3eb3217d6f3552563e
petercolella <peter.joseph.colella@gmail.com>
2019-02-18 22:48:17 -0500

    replaced single index.js file in client with separate index & App files

---
+++ client/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+//components
+import About from './components/MainBody/About/About';
+import Landing from './components/MainBody/Landing';
+import MainBody from './components/MainBody/MainBody';
+import RegistrationWizard from './components/MainBody/RegistrationWizard/RegistrationWizard';
+import Profile from './components/MainBody/Profile';
+import Birthday from './components/QuestionComponents/Birthday';
+import Nudges from './components/QuestionComponents/Nudges';
+import Partner from './components/QuestionComponents/Partner';
+import Phone from './components/QuestionComponents/Phone';
+import Anniversary from './components/QuestionComponents/Anniversary';
+import NudgeDB from './components/MainBody/Nudgedb';
+
+//CSS
+import './styles.css';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/dashboard" component={MainBody} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/registration" component={RegistrationWizard} />
+        <Route exact path="/profile" component={Profile} />
+        <Route exact path="/birthday" component={Birthday} />
+        <Route exact path="/nudges" component={Nudges} />
+        <Route exact path="/partner" component={Partner} />
+        <Route exact path="/phone" component={Phone} />
+        <Route exact path="/anniversary" component={Anniversary} />
+        <Route exact path="/nudgedb" component={NudgeDB} />
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
