b798a339d73834021379297c01cf85b96030fcab
petercolella <peter.joseph.colella@gmail.com>
2019-02-26 10:03:54 -0500

    able to get google id_token on sign in

--- client/src/components/MainBody/Signin/Google.js
+++ client/src/components/MainBody/Signin/Google.js
@@ -33,6 +33,8 @@ class SignIn extends Component {
 
   onSignIn = googleUser => {
     var profile = googleUser.getBasicProfile();
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log('ID_token: ' + id_token);
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
