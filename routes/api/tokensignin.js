874d1b29ebf76722f3acd07fcd6e51d1618f4728
petercolella <peter.joseph.colella@gmail.com>
2019-02-27 21:06:42 -0500

    config file for CLIENT_ID

--- routes/api/tokensignin.js
+++ routes/api/tokensignin.js
@@ -1,10 +1,15 @@
 const router = require('express').Router();
+const { OAuth2Client } = require('google-auth-library');
+const config = require('../../config/config.json');
+const CLIENT_ID = config.development.CLIENT_ID;
+const client = new OAuth2Client(CLIENT_ID);
 
 router.route('/').post(function(req, res) {
   const token_id = req.body.idtoken;
-  console.log(req.body);
-  console.log('Backend route recieved token_id: ', token_id);
+  //   console.log(req.body);
+  //   console.log('Backend route recieved token_id: ', token_id);
   //   res.sendStatus(200);
+  console.log(CLIENT_ID);
   res.send('New User');
 });
 
