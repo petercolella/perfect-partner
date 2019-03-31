0ef024bc0618da76312ec7479179984d2c21e14b
petercolella <peter.joseph.colella@gmail.com>
2019-03-31 15:45:36 -0400

    labels added to edit nudge modal

--- client/src/components/MainBody/NudgeUpdate/index.js
+++ client/src/components/MainBody/NudgeUpdate/index.js
@@ -14,23 +14,29 @@ const NudgeUpdate = props => (
           <form>
             <div className="form-group">
               <div className="col">
+                <label htmlFor="nudgeNameInput">Name</label>
                 <input
                   type="text"
                   className="form-control"
+                  id="nudgeNameInput"
                   onChange={props.handleInputChange}
                   value={props.nudge.name}
                   name="name"
                 />
+                <label htmlFor="nudgeFrequencyInput">Frequency</label>
                 <input
                   type="text"
                   className="form-control"
+                  id="nudgeFrequencyInput"
                   onChange={props.handleInputChange}
                   value={props.nudge.nudgeFrequency}
                   name="nudgeFrequency"
                 />
+                <label htmlFor="nudgeTextInput">Text Body</label>
                 <input
                   type="text"
                   className="form-control"
+                  id="nudgeTextInput"
                   onChange={props.handleInputChange}
                   value={props.nudge.textMessage}
                   name="textMessage"
