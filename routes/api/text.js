1c99b4101219b8882a0025c455639b3229aee690
petercolella <peter.joseph.colella@gmail.com>
2019-06-23 21:56:22 -0400

    adds textController.js

--- routes/api/text.js
+++ routes/api/text.js
@@ -1,60 +1,6 @@
-require('dotenv').config();
 const router = require('express').Router();
-const nodemailer = require('nodemailer');
+const textController = require('../../controllers/textController');
 
-// Download the helper library from https://www.twilio.com/docs/node/install
-// Your Account Sid and Auth Token from twilio.com/console
-// DANGER! This is insecure. See http://twil.io/secure
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken);
-
-router.post('/send', (req, res) => {
-  const phone = req.body.phone;
-  const textMessage = req.body.textMessage;
-
-  client.messages
-    .create({
-      body: `${textMessage}`,
-      from: process.env.TWILIO_PHONE_NUMBER,
-      to: `+1${phone}`
-    })
-    .then(message => {
-      console.log(message.sid);
-      res.json({ msg: 'Success' });
-    });
-
-  //   const mail = {
-  //     from: '"Perfect Partner" <perfect@anthonylindo.com>',
-  //     to: `${phone}@textmagic.com`,
-  //     subject: 'Hello ✔',
-  //     text: `${textMessage}`
-  //   };
-
-  //   const transporter = nodemailer.createTransport({
-  //     host: 'mail.anthonylindo.com',
-  //     port: 465,
-  //     secure: true,
-  //     auth: {
-  //       user: 'perfect@anthonylindo.com',
-  //       pass: 'Trigoph_9'
-  //     }
-  //   });
-
-  //   transporter.sendMail(mail, (err, data) => {
-  //     if (err) {
-  //       res.json({
-  //         msg: 'Fail'
-  //       });
-  //       return console.log(err);
-  //     } else {
-  //       console.log('Message sent: %s', data.messageId);
-  //       console.log('Preview URL: %s', nodemailer.getTestMessageUrl(data));
-  //       res.json({
-  //         msg: 'Success'
-  //       });
-  //     }
-  //   });
-});
+router.route('/send').post(textController.send);
 
 module.exports = router;
