c9487a5ac9a82615ca54bf993353ca0b7c2faf54
petercolella <peter.joseph.colella@gmail.com>
2019-08-05 16:13:59 -0400

    adds card to MainBody; conversion to hooks

--- client/src/components/MainBody/index.js
+++ client/src/components/MainBody/index.js
@@ -1,14 +1,36 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 import API from '../../utils/API';
-// import Dashboard from '../Dashboard';
 import UserUpdate from '../UserUpdate';
 import NudgeTable from '../NudgeTable';
 import fn from '../../utils/fn';
 const $ = window.$;
 
-class MainBody extends Component {
-  state = {
+const useStyles = makeStyles({
+  card: {
+    minWidth: 275
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)'
+  },
+  title: {
+    fontSize: 14
+  },
+  pos: {
+    marginBottom: 12
+  }
+});
+
+const MainBody = props => {
+  const [state, setState] = useState({
     User: {
       name: '',
       firstName: '',
@@ -30,214 +52,253 @@ class MainBody extends Component {
       activated: false
     },
     nudgeFrequencyUnit: ''
-  };
+  });
 
-  componentDidMount() {
-    const path = this.props.location.pathname;
-    this.props.setPreviousPath(path);
-    this.loadUserInfo();
-  }
+  console.log('props', props);
+  console.log('state', state);
 
-  loadUserInfo = () => {
+  //   componentDidMount() {
+  //     const path = this.props.location.pathname;
+  //     this.props.setPreviousPath(path);
+  //     this.loadUserInfo();
+  //   }
+
+  useEffect(() => {
+    const path = props.location.pathname;
+    props.setPreviousPath(path);
+    loadUserInfo();
+  }, []);
+
+  const loadUserInfo = () => {
     const id = sessionStorage.getItem('currentUserId');
+    console.log('id', id);
     API.getUser(id).then(res => {
-      this.setState(
+      console.log('res.data', res.data);
+      setState(
         res.data
-          ? { User: res.data, nudges: res.data.nudges }
-          : { state: this.prevState }
+          ? {
+              User: res.data,
+              nudges: res.data.nudges,
+              nudge: state.nudge,
+              nudgeFrequencyUnit: state.nudgeFrequencyUnit
+            }
+          : { state: state }
       );
     });
   };
 
-  launchUpdateComp = nudge => {
-    this.setState({ nudge });
-    this.showModal();
+  const launchUpdateComp = nudge => {
+    setState({ nudge });
+    showModal();
   };
 
-  showModal() {
+  const showModal = () => {
     $('#editNudgeModalCenter').modal('show');
-  }
-
-  closeUpdateComp = () => {
-    this.hideModal();
   };
 
-  hideModal() {
+  const closeUpdateComp = () => {
+    hideModal();
+  };
+
+  const hideModal = () => {
     $('#editNudgeModalCenter').modal('hide');
-  }
-
-  launchUserUpdateComp = User => {
-    this.setState({ User });
-    this.showUserModal();
   };
 
-  showUserModal() {
+  const launchUserUpdateComp = User => {
+    setState({ User });
+    showUserModal();
+  };
+
+  const showUserModal = () => {
     $('#editUserModalCenter').modal('show');
-  }
-
-  closeUserUpdateComp = () => {
-    this.hideUserModal();
   };
 
-  hideUserModal() {
-    $('#editUserModalCenter').modal('hide');
-  }
+  const closeUserUpdateComp = () => {
+    hideUserModal();
+  };
 
-  handleInputChange = event => {
+  const hideUserModal = () => {
+    $('#editUserModalCenter').modal('hide');
+  };
+
+  const handleInputChange = event => {
     const { name, value } = event.target;
-    this.setState({
+    setState({
       nudge: {
-        ...this.state.nudge,
+        ...state.nudge,
         [name]: value
       }
     });
   };
 
-  handleFormSubmit = event => {
+  const handleFormSubmit = event => {
     event.preventDefault();
     $('#nudge-toast').toast('show');
-    API.updateNudge(this.state.nudge._id, {
-      ...this.state.nudge
+    API.updateNudge(state.nudge._id, {
+      ...state.nudge
     }).then(() => {
-      this.loadUserInfo();
+      loadUserInfo();
     });
   };
 
-  handleUserInputChange = event => {
+  const handleUserInputChange = event => {
     const { name, value } = event.target;
-    this.setState({
+    setState({
       User: {
-        ...this.state.User,
+        ...state.User,
         [name]: value
       }
     });
   };
 
-  handleUserFormSubmit = event => {
+  const handleUserFormSubmit = event => {
     event.preventDefault();
     $('#user-toast').toast('show');
-    API.updateUser(this.state.User._id, {
-      ...this.state.User
+    API.updateUser(state.User._id, {
+      ...state.User
     }).then(() => {
-      this.loadUserInfo();
+      loadUserInfo();
     });
   };
 
-  render() {
-    return (
-      <div className="container-fluid">
-        <div className="row ">
-          <div className="col-md-4 col-sm-12">
-            <img id="header-img" alt="logo" src="/img/logo_p.png" />
-          </div>
-          <div className="col-md-4 offset-md-4 col-sm-12 d-flex justify-content-end">
-            <h3 className="my-auto mr-2" id="avatar-text-header">
-              {this.state.User.name ? (
-                this.state.User.name
-              ) : (
-                <span>Please sign in.</span>
-              )}
-            </h3>
-            <img
-              className="my-auto"
-              id="avatar-image-header"
-              alt="User"
-              src={this.state.User.imageUrl}
-            />
-          </div>
+  const classes = useStyles();
+
+  return (
+    <div className="container-fluid">
+      <div className="row ">
+        <div className="col-md-4 col-sm-12">
+          <img id="header-img" alt="logo" src="/img/logo_p.png" />
         </div>
-        <div className="row">
-          <div className="col-md-12 dashboard-rght">
-            <div className="row" style={{ padding: '1em' }}>
-              {/* <div className="col-md-2  avatar-center dash">
-                <h1>{this.state.User.name}</h1>
-                <img
-                  id="avatar-image"
-                  alt="User"
-                  src={this.state.User.imageUrl}
-                />
-              </div> */}
-              <div
-                className="col-md-6 col-sm-12 d-flex align-items-center justify-content-center p-1"
-                style={{ border: 'white solid 0.25rem' }}>
-                {this.state.User.name ? (
-                  <div>
-                    <p>
-                      <span>Phone Number: </span>
-                      {this.state.User.phone
-                        ? fn.formatPhoneNumber(this.state.User.phone)
-                        : null}
-                    </p>
-                    <p>
-                      <span>Partner's Name: </span>
-                      {this.state.User.partnerName}
-                    </p>
-                    <p>
-                      <span>Partner's Birthday: </span>
-                      {this.state.User.birthDate}
-                    </p>
-                    <p>
-                      <span>Your Anniversary: </span>
-                      {this.state.User.anniversaryDate}
-                    </p>
-                    <button
-                      className="btn btn-primary"
-                      onClick={() =>
-                        this.launchUserUpdateComp(this.state.User)
-                      }>
-                      Edit Your Profile
-                    </button>
-                  </div>
-                ) : (
+        <div className="col-md-4 offset-md-4 col-sm-12 d-flex justify-content-end">
+          <h3 className="my-auto mr-2" id="avatar-text-header">
+            {state.User.name ? state.User.name : <span>Please sign in.</span>}
+          </h3>
+          <img
+            className="my-auto"
+            id="avatar-image-header"
+            alt="User"
+            src={state.User.imageUrl}
+          />
+        </div>
+      </div>
+      <div className="row">
+        {state.User.name ? (
+          <Card className={classes.card}>
+            <CardContent>
+              <Typography
+                className={classes.title}
+                color="textSecondary"
+                gutterBottom>
+                <span>Phone Number: </span>
+                {state.User.phone
+                  ? fn.formatPhoneNumber(state.User.phone)
+                  : null}
+              </Typography>
+              <Typography variant="h5" component="h2">
+                <span>Partner's Name: </span>
+                {state.User.partnerName}
+              </Typography>
+              <Typography className={classes.pos} color="textSecondary">
+                <span>Partner's Birthday: </span>
+                {state.User.birthDate}
+              </Typography>
+              <Typography variant="body2" component="p">
+                <span>Your Anniversary: </span>
+                {state.User.anniversaryDate}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button onClick={() => launchUserUpdateComp(state.User)}>
+                Edit Your Profile
+              </Button>
+            </CardActions>
+          </Card>
+        ) : (
+          <Card className={classes.card}>
+            <CardContent>
+              <Typography
+                className={classes.title}
+                color="textSecondary"
+                gutterBottom>
+                Please click{' '}
+                <Link to="/" style={{ color: '#22b5e0' }}>
+                  here
+                </Link>{' '}
+                to sign in before continuing.
+              </Typography>
+            </CardContent>
+          </Card>
+        )}
+        <div className="col-md-12 dashboard-rght">
+          <div className="row" style={{ padding: '1em' }}>
+            <div
+              className="col-md-6 col-sm-12 d-flex align-items-center justify-content-center p-1"
+              style={{ border: 'white solid 0.25rem' }}>
+              {state.User.name ? (
+                <div>
                   <p>
-                    Please click{' '}
-                    <Link to="/" style={{ color: '#22b5e0' }}>
-                      here
-                    </Link>{' '}
-                    to sign in before continuing.
+                    <span>Phone Number: </span>
+                    {state.User.phone
+                      ? fn.formatPhoneNumber(state.User.phone)
+                      : null}
                   </p>
-                )}
-              </div>
-              <div className="col-md-6 col-sm-12 p-1">
-                <span className="helper" />
-                <img alt="love-hearts" src="./img/love-hearts.jpg" />
-              </div>
+                  <p>
+                    <span>Partner's Name: </span>
+                    {state.User.partnerName}
+                  </p>
+                  <p>
+                    <span>Partner's Birthday: </span>
+                    {state.User.birthDate}
+                  </p>
+                  <p>
+                    <span>Your Anniversary: </span>
+                    {state.User.anniversaryDate}
+                  </p>
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => launchUserUpdateComp(state.User)}>
+                    Edit Your Profile
+                  </button>
+                </div>
+              ) : (
+                <p>
+                  Please click{' '}
+                  <Link to="/" style={{ color: '#22b5e0' }}>
+                    here
+                  </Link>{' '}
+                  to sign in before continuing.
+                </p>
+              )}
+            </div>
+            <div className="col-md-6 col-sm-12 p-1">
+              {/* <span className="helper" />
+                <img alt="love-hearts" src="./img/love-hearts.jpg" /> */}
             </div>
           </div>
         </div>
-        <div className="row">
-          <div className="col-md-12">
-            {/* <Dashboard
-              user={this.state.User}
-              nudges={this.state.nudges}
-              nudge={this.state.nudge}
-              loadUserInfo={this.loadUserInfo}
-              launchUpdateComp={this.launchUpdateComp}
-              closeUpdateComp={this.closeUpdateComp}
-              handleInputChange={this.handleInputChange}
-              handleFormSubmit={this.handleFormSubmit}
-            /> */}
-            <NudgeTable
-              user={this.state.User}
-              nudges={this.state.nudges}
-              nudge={this.state.nudge}
-              loadUserInfo={this.loadUserInfo}
-              launchUpdateComp={this.launchUpdateComp}
-              closeUpdateComp={this.closeUpdateComp}
-              handleInputChange={this.handleInputChange}
-              handleFormSubmit={this.handleFormSubmit}
-            />
-          </div>
-        </div>
-        <UserUpdate
-          closeUserUpdateComp={this.closeUserUpdateComp}
-          handleUserInputChange={this.handleUserInputChange}
-          handleUserFormSubmit={this.handleUserFormSubmit}
-          user={this.state.User}
-        />
       </div>
-    );
-  }
-}
+      <div className="row">
+        <div className="col-md-12">
+          <NudgeTable
+            user={state.User}
+            nudges={state.nudges}
+            nudge={state.nudge}
+            loadUserInfo={loadUserInfo}
+            launchUpdateComp={launchUpdateComp}
+            closeUpdateComp={closeUpdateComp}
+            handleInputChange={handleInputChange}
+            handleFormSubmit={handleFormSubmit}
+          />
+        </div>
+      </div>
+      <UserUpdate
+        closeUserUpdateComp={closeUserUpdateComp}
+        handleUserInputChange={handleUserInputChange}
+        handleUserFormSubmit={handleUserFormSubmit}
+        user={state.User}
+      />
+    </div>
+  );
+};
 
 export default MainBody;
