ccb3195adece7222f9e9080700719dfc25fa6b37
petercolella <peter.joseph.colella@gmail.com>
2019-08-16 21:57:21 -0400

    adds svg credit to NudgeTable

--- client/src/components/NudgeTable/index.js
+++ client/src/components/NudgeTable/index.js
@@ -87,6 +87,23 @@ const NudgeTable = props => {
           </TableBody>
         </Table>
       </Paper>
+      {/* <div>
+        Icons made by{' '}
+        <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
+          Freepik
+        </a>{' '}
+        from{' '}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>{' '}
+        is licensed by{' '}
+        <a
+          href="http://creativecommons.org/licenses/by/3.0/"
+          title="Creative Commons BY 3.0"
+          target="_blank">
+          CC 3.0 BY
+        </a>
+      </div> */}
       <div aria-live="polite" aria-atomic="true">
         <div
           className="toast "
