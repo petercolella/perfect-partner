26048c16e9fe54777d86544449051daba21df49b
petercolella <peter.joseph.colella@gmail.com>
2019-08-18 23:47:53 -0400

    adds material-ui dialog to UserUpdate - wip

--- client/src/components/UserUpdate/index.js
+++ client/src/components/UserUpdate/index.js
@@ -1,8 +1,151 @@
 import React from 'react';
+import clsx from 'clsx';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import CloseIcon from '@material-ui/icons/Close';
+import { green } from '@material-ui/core/colors';
+import IconButton from '@material-ui/core/IconButton';
+import Snackbar from '@material-ui/core/Snackbar';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
+import { makeStyles } from '@material-ui/core/styles';
+import Slide from '@material-ui/core/Slide';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+const useStyles1 = makeStyles(theme => ({
+  success: {
+    backgroundColor: green[600]
+  },
+  icon: {
+    fontSize: 20
+  },
+  iconVariant: {
+    opacity: 0.9,
+    marginRight: theme.spacing(1)
+  },
+  message: {
+    display: 'flex',
+    alignItems: 'center'
+  }
+}));
+
+const MySnackbarContentWrapper = React.forwardRef((props, ref) => {
+  const classes = useStyles1();
+  const { message, onClose, variant, ...other } = props;
+
+  return (
+    <SnackbarContent
+      className={clsx(classes[variant])}
+      aria-describedby="client-snackbar"
+      message={
+        <span id="client-snackbar" className={classes.message}>
+          <CheckCircleIcon
+            className={clsx(classes.icon, classes.iconVariant)}
+          />
+          {message}
+        </span>
+      }
+      action={[
+        <IconButton
+          key="close"
+          aria-label="close"
+          color="inherit"
+          onClick={onClose}>
+          <CloseIcon className={classes.icon} />
+        </IconButton>
+      ]}
+      {...other}
+      ref={ref}
+    />
+  );
+});
+
+function TransitionUp(props) {
+  return <Slide {...props} direction="up" />;
+}
 
 const UserUpdate = props => {
+  const [toastOpen, setToastOpen] = React.useState(false);
+
+  function handleToastClose(event, reason) {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setToastOpen(false);
+  }
+
+  const clickHandler = e => {
+    props.handleFormSubmit(e);
+    setToastOpen(true);
+  };
+
   return (
     <div>
+      <div>
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left'
+          }}
+          open={toastOpen}
+          autoHideDuration={2000}
+          onClose={handleToastClose}
+          TransitionComponent={TransitionUp}
+          ContentProps={{
+            'aria-describedby': 'message-id'
+          }}>
+          <MySnackbarContentWrapper
+            onClose={handleToastClose}
+            variant="success"
+            message={
+              <span>
+                Your profile has been successfully updated,{' '}
+                {props.user.firstName}.
+              </span>
+            }
+          />
+        </Snackbar>
+      </div>
+      <Dialog
+        fullWidth={true}
+        open={props.userDialogOpen}
+        onClose={props.closeUpdateComp}
+        aria-labelledby="form-dialog-title"
+        scroll={'body'}>
+        <DialogTitle id="form-dialog-title">
+          Edit Your Profile, {props.user.firstName}.
+          {/* <Pencil height="2.5em" width="2.5em" style={{ marginLeft: 16 }} /> */}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Make changes below. Your email address is from the Gmail account
+            with which you logged in.
+          </DialogContentText>
+          <TextField
+            id="name"
+            label="Full Name"
+            type="text"
+            fullWidth
+            value={props.user.name}
+            onChange={props.handleUserInputChange('name')}
+            margin="normal"
+            variant="outlined"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={props.closeUserUpdateComp} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={e => clickHandler(e)} color="primary">
+            Submit
+          </Button>
+        </DialogActions>
+      </Dialog>
       <div
         className="modal fade"
         id="editUserModalCenter"
