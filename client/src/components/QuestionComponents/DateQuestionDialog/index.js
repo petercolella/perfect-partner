22543121ecdbbae43f06d7b581dfa1c78fa3570e
petercolella <peter.joseph.colella@gmail.com>
2019-08-26 18:39:37 -0400

    adds DateQuestionDialog component

---
+++ client/src/components/QuestionComponents/DateQuestionDialog/index.js
@@ -0,0 +1,166 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import clsx from 'clsx';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import CloseIcon from '@material-ui/icons/Close';
+import { green } from '@material-ui/core/colors';
+import IconButton from '@material-ui/core/IconButton';
+import Snackbar from '@material-ui/core/Snackbar';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
+import { makeStyles } from '@material-ui/core/styles';
+import Slide from '@material-ui/core/Slide';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import 'date-fns';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker
+} from '@material-ui/pickers';
+// import { ReactComponent as Pencil } from './pencil.svg';
+
+const useStyles1 = makeStyles(theme => ({
+  success: {
+    backgroundColor: green[600]
+  },
+  icon: {
+    fontSize: 20
+  },
+  iconVariant: {
+    opacity: 0.9,
+    marginRight: theme.spacing(1)
+  },
+  message: {
+    display: 'flex',
+    alignItems: 'center'
+  }
+}));
+
+const MySnackbarContentWrapper = React.forwardRef((props, ref) => {
+  const classes = useStyles1();
+  const { message, onClose, variant, ...other } = props;
+
+  return (
+    <SnackbarContent
+      className={clsx(classes[variant])}
+      aria-describedby="client-snackbar"
+      message={
+        <span id="client-snackbar" className={classes.message}>
+          <CheckCircleIcon
+            className={clsx(classes.icon, classes.iconVariant)}
+          />
+          {message}
+        </span>
+      }
+      action={[
+        <IconButton
+          key="close"
+          aria-label="close"
+          color="inherit"
+          onClick={onClose}>
+          <CloseIcon className={classes.icon} />
+        </IconButton>
+      ]}
+      {...other}
+      ref={ref}
+    />
+  );
+});
+
+function TransitionUp(props) {
+  return <Slide {...props} direction="up" />;
+}
+
+const DateQuestionDialog = props => {
+  const [toastOpen, setToastOpen] = React.useState(false);
+
+  function handleToastClose(event, reason) {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setToastOpen(false);
+  }
+
+  const clickHandler = e => {
+    props.handleFormSubmit(e);
+    setToastOpen(true);
+  };
+
+  return (
+    <div>
+      <div>
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left'
+          }}
+          open={toastOpen}
+          autoHideDuration={2000}
+          onClose={handleToastClose}
+          TransitionComponent={TransitionUp}
+          ContentProps={{
+            'aria-describedby': 'message-id'
+          }}>
+          <MySnackbarContentWrapper
+            onClose={handleToastClose}
+            variant="success"
+            message={
+              <span>
+                {props.title}: {props.userField} has been submitted.
+              </span>
+            }
+          />
+        </Snackbar>
+      </div>
+      <Dialog
+        fullWidth={true}
+        open={true}
+        onClose={props.closeUpdateComp}
+        aria-labelledby="form-dialog-title"
+        scroll={'body'}>
+        <DialogTitle id="form-dialog-title">
+          {/* <Pencil height="2.5em" width="2.5em" style={{ marginRight: 16 }} /> */}
+          {props.user.firstName}, {props.question}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Give it your special touch. You can make changes as often as you
+            like!
+          </DialogContentText>
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <KeyboardDatePicker
+              autoOk
+              id="birthDate"
+              label="Partner's Birthday"
+              format="MM/dd/yyyy"
+              fullWidth
+              value={props.user.birthDate}
+              onChange={props.handleUserDateInputChange('birthDate')}
+              KeyboardButtonProps={{
+                'aria-label': 'change date'
+              }}
+              margin="normal"
+              variant="inline"
+              inputVariant="outlined"
+            />
+          </MuiPickersUtilsProvider>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={clickHandler} color="primary">
+            Submit
+          </Button>
+          <Link to={props.link}>
+            <Button color="primary">Next</Button>
+          </Link>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+export default DateQuestionDialog;
