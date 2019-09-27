52a5f6af274f58e2d29939e61419640ae4c8f3d7
petercolella <peter.joseph.colella@gmail.com>
2019-09-26 23:58:52 -0400

    updates birthday question component to use hooks

--- client/src/components/QuestionComponents/Birthday/index.js
+++ client/src/components/QuestionComponents/Birthday/index.js
@@ -1,79 +1,85 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import API from '../../../utils/API';
 import Header from '../../Header';
 import DateQuestionDialog from '../DateQuestionDialog';
 import { ReactComponent as Cake } from './cake.svg';
 
-class Birthday extends Component {
-  state = {
+const Birthday = props => {
+  const [state, setState] = useState({
     User: {},
     title: 'Birthday',
     question: "what is your partner's birthday?",
     userField: null,
     nextQuestionLink: '/anniversary'
-  };
+  });
 
-  componentDidMount() {
-    this.loadUserInfo();
-  }
-
-  componentWillUnmount() {
-    const path = this.props.location.pathname;
-    this.props.setPreviousPath(path);
-  }
-
-  loadUserInfo = () => {
+  const loadUserInfo = useCallback(() => {
     const id = sessionStorage.getItem('currentUserId');
     if (id) {
       API.getUser(id).then(res => {
-        this.setState({
+        setState(state => ({
+          ...state,
           User: res.data,
-          userField: res.data.birthDate
-        });
+          userField: res.data.anniversaryDate
+        }));
       });
     }
+  }, []);
+
+  useEffect(() => {
+    loadUserInfo();
+
+    return () => {
+      const path = props.location.pathname;
+      props.setPreviousPath(path);
+    };
+  }, [loadUserInfo, props]);
+
+  const closeDateQuestionDialog = () => {
+    loadUserInfo();
   };
 
-  handleFormSubmit = event => {
+  const handleFormSubmit = event => {
     event.preventDefault();
-    API.updateUser(this.state.User._id, {
-      birthDate: this.state.userField
-    }).then(this.loadUserInfo);
+    API.updateUser(state.User._id, {
+      birthDate: state.userField
+    }).then(loadUserInfo);
   };
 
-  handleUserDateInputChange = date => {
-    this.setState({
-      userField: date
+  const handleUserDateInputChange = name => date => {
+    console.log('date:', date);
+    setState({
+      ...state,
+      [name]: date
     });
   };
 
-  render() {
-    return (
-      <>
-        <Header />
-        <div className="bkgd-image">
-          <div className="container invisible">
-            <div className="row mb-3">
-              <div className="col-md-4 col-sm-12">
-                <img id="header-img" alt="logo" src="/img/logo_p.png" />
-              </div>
+  return (
+    <>
+      <Header />
+      <div className="bkgd-image">
+        <div className="container invisible">
+          <div className="row mb-3">
+            <div className="col-md-4 col-sm-12">
+              <img id="header-img" alt="logo" src="/img/logo_p.png" />
             </div>
           </div>
-          <DateQuestionDialog
-            firstName={this.state.User.firstName}
-            title={this.state.title}
-            question={this.state.question}
-            userField={this.state.userField}
-            link={this.state.nextQuestionLink}
-            handleFormSubmit={this.handleFormSubmit}
-            handleUserDateInputChange={this.handleUserDateInputChange}
-            image={Cake}
-            label="Partner's Birthday"
-          />
         </div>
-      </>
-    );
-  }
-}
+        <DateQuestionDialog
+          firstName={state.User.firstName}
+          title={state.title}
+          question={state.question}
+          userField={state.userField}
+          link={state.nextQuestionLink}
+          closeDateQuestionDialog={closeDateQuestionDialog}
+          handleFormSubmit={handleFormSubmit}
+          handleUserDateInputChange={handleUserDateInputChange}
+          image={Cake}
+          label="Partner's Birthday"
+        />
+      </div>
+    </>
+  );
+};
 
 export default Birthday;
