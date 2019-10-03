40f68ab75a67540aa095761a40f95d13810a0744
petercolella <peter.joseph.colella@gmail.com>
2019-10-02 23:24:08 -0400

    adds snackbar to NudgeDialog

--- client/src/components/QuestionComponents/NudgeDialog/index.js
+++ client/src/components/QuestionComponents/NudgeDialog/index.js
@@ -1,6 +1,17 @@
 import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import ErrorIcon from '@material-ui/icons/Error';
+import InfoIcon from '@material-ui/icons/Info';
+import CloseIcon from '@material-ui/icons/Close';
+import { amber, green } from '@material-ui/core/colors';
+import IconButton from '@material-ui/core/IconButton';
+import Snackbar from '@material-ui/core/Snackbar';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
+import WarningIcon from '@material-ui/icons/Warning';
+import Slide from '@material-ui/core/Slide';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -27,6 +38,73 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
+const variantIcon = {
+  success: CheckCircleIcon,
+  warning: WarningIcon,
+  error: ErrorIcon,
+  info: InfoIcon
+};
+
+const useStyles1 = makeStyles(theme => ({
+  success: {
+    backgroundColor: green[600]
+  },
+  error: {
+    backgroundColor: theme.palette.error.dark
+  },
+  info: {
+    backgroundColor: theme.palette.primary.main
+  },
+  warning: {
+    backgroundColor: amber[700]
+  },
+  icon: {
+    fontSize: 20
+  },
+  iconVariant: {
+    opacity: 0.9,
+    marginRight: theme.spacing(1)
+  },
+  message: {
+    display: 'flex',
+    alignItems: 'center'
+  }
+}));
+
+const MySnackbarContentWrapper = React.forwardRef((props, ref) => {
+  const classes = useStyles1();
+  const { className, message, onClose, variant, ...other } = props;
+  const Icon = variantIcon[variant];
+
+  return (
+    <SnackbarContent
+      className={clsx(classes[variant])}
+      aria-describedby="client-snackbar"
+      message={
+        <span id="client-snackbar" className={classes.message}>
+          <Icon className={clsx(classes.icon, classes.iconVariant)} />
+          {message}
+        </span>
+      }
+      action={[
+        <IconButton
+          key="close"
+          aria-label="close"
+          color="inherit"
+          onClick={onClose}>
+          <CloseIcon className={classes.icon} />
+        </IconButton>
+      ]}
+      {...other}
+      ref={ref}
+    />
+  );
+});
+
+function TransitionUp(props) {
+  return <Slide {...props} direction="up" />;
+}
+
 const NudgeDialog = props => {
   const classes = useStyles();
 
@@ -37,6 +115,7 @@ const NudgeDialog = props => {
     'Dinner Reservations'
   ]);
   const [snackbarNudges, setSnackbarNudges] = useState([]);
+  const [toastOpen, setToastOpen] = useState(false);
   const [state, setState] = useState({});
 
   React.useEffect(() => {
@@ -68,18 +147,6 @@ const NudgeDialog = props => {
     setState({ ...state, [name]: event.target.checked });
   };
 
-  function handleDialogClose(event, reason) {
-    if (reason === 'clickaway') {
-      return;
-    }
-
-    setDialogOpen(false);
-    props.loadUserInfo();
-    setTimeout(() => {
-      setDialogOpen(true);
-    }, 250);
-  }
-
   const selectAllCheckboxes = isSelected => {
     Object.keys(state).forEach(name => {
       setState(prevState => ({
@@ -110,10 +177,66 @@ const NudgeDialog = props => {
       });
 
     setSnackbarNudges(newNudges);
+    setToastOpen(true);
+  }
+
+  function handleDialogClose(event, reason) {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setDialogOpen(false);
+    props.loadUserInfo();
+    setTimeout(() => {
+      setDialogOpen(true);
+    }, 250);
+  }
+
+  function handleToastClose(event, reason) {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setToastOpen(false);
   }
 
   return (
     <div>
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left'
+        }}
+        open={toastOpen}
+        autoHideDuration={3000}
+        onClose={handleToastClose}
+        TransitionComponent={TransitionUp}
+        ContentProps={{
+          'aria-describedby': 'message-id'
+        }}>
+        {snackbarNudges.length > 0 ? (
+          <MySnackbarContentWrapper
+            onClose={handleToastClose}
+            variant="success"
+            message={
+              <span>
+                You submitted these nudges:
+                <ul>
+                  {snackbarNudges.map((nudge, i) => (
+                    <li key={i}>{nudge}</li>
+                  ))}
+                </ul>
+              </span>
+            }
+          />
+        ) : (
+          <MySnackbarContentWrapper
+            onClose={handleToastClose}
+            variant="warning"
+            message={<span>Oops!</span>}
+          />
+        )}
+      </Snackbar>
       <Dialog
         fullWidth={true}
         open={dialogOpen}
