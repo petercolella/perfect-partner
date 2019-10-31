470a39cabffa5f71a4e5e459e9a3fa19c79def84
petercolella <peter.joseph.colella@gmail.com>
2019-10-31 17:48:12 -0400

    removes Google functions from NavBar

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,6 +1,5 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import API from '../../utils/API';
 
 import AppBar from '@material-ui/core/AppBar';
 import Grid from '@material-ui/core/Grid';
@@ -31,55 +30,7 @@ const useStyles = makeStyles(theme => ({
 const NavBar = props => {
   const classes = useStyles();
 
-  const { user, getPreviousPath, setUser, signedIn, signOut } = props;
-
-  useEffect(() => {
-    const onSignIn = googleUser => {
-      const profile = googleUser.getBasicProfile();
-      const id_token = googleUser.getAuthResponse().id_token;
-      console.log('ID: ' + profile.getId());
-      console.log('Email: ' + profile.getEmail());
-
-      API.tokenSignInAxios(id_token).then(id => {
-        sessionStorage.setItem('currentUserId', id);
-        API.getUser(id).then(res => {
-          setUser(user => (res.data ? res.data : user));
-        });
-      });
-    };
-
-    const onSuccess = googleUser => {
-      console.log('Signed in as: ' + googleUser.getBasicProfile().getName());
-      onSignIn(googleUser);
-    };
-
-    const onFailure = error => {
-      console.log(error);
-    };
-
-    const renderGoogleLoginButton = () => {
-      if (!signedIn) {
-        console.log('rendering google signin button');
-        window.gapi.signin2.render('my-signin2', {
-          scope: 'profile email',
-          width: 180,
-          height: 30,
-          longtitle: true,
-          theme: 'dark',
-          onsuccess: onSuccess,
-          onfailure: onFailure
-        });
-      }
-    };
-
-    window.addEventListener('google-loaded', renderGoogleLoginButton);
-    const previousPath = getPreviousPath();
-    console.log('previousPathGoogle:', previousPath);
-    if (previousPath) {
-      console.log('previousPath:', true);
-      renderGoogleLoginButton();
-    }
-  }, [getPreviousPath, setUser, signedIn]);
+  const { user, signedIn, signOut } = props;
 
   return (
     <div className={classes.root}>
