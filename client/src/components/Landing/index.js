a35f8df2da1304e869807ed9d3a217ed68475bc9
petercolella <peter.joseph.colella@gmail.com>
2019-11-04 20:50:05 -0500

    converts Landing component to Material-UI without any use of Bootstrap or an external stylesheet

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -1,28 +1,71 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import Avatar from '../Avatar';
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    height: '100%'
+  },
+  homeSaying: {
+    color: '#fff',
+    padding: theme.spacing(4),
+    textShadow: '2px 2px #848484'
+  },
+  paper: {
+    borderRadius: 25,
+    backgroundColor: '#ef5350',
+    marginBottom: theme.spacing(4),
+    padding: theme.spacing(4)
+  }
+}));
 
 const Landing = props => {
   const { user } = props;
 
+  const classes = useStyles();
+
   return (
     <>
       <div className="bkgd-image">
-        <Toolbar />
-        <div className="container">
-          <div className="row justify-content-end">
-            <div className="col-md-6 col-sm-12">
-              <p className="home-saying">Planned Spontaneity</p>
-            </div>
-          </div>
-          <div className="row bottom-xs">
-            <div className="col-xs-6">
-              <div className="box shadow">
-                <Avatar user={user} />
-              </div>
-            </div>
-          </div>
-        </div>
+        <Container className={classes.container}>
+          <Grid
+            container
+            direction="column"
+            justify="space-between"
+            className={classes.container}>
+            <Grid
+              container
+              direction="row"
+              justify="flex-end"
+              alignItems="flex-start">
+              <Grid item xs={12} md={6}>
+                <Toolbar />
+                <Typography
+                  variant="h2"
+                  align="right"
+                  className={classes.homeSaying}>
+                  Planned Spontaneity
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid
+              container
+              direction="row"
+              justify="flex-start"
+              alignItems="flex-end">
+              <Grid item>
+                <Paper elevation={12} className={classes.paper}>
+                  <Avatar user={user} />
+                </Paper>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Container>
       </div>
     </>
   );
