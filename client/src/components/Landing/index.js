101d526d944a92e389c10c65cdabfd73cd800b8f
petercolella <peter.joseph.colella@gmail.com>
2019-11-08 19:03:36 -0500

    moves Paper import from Landing to Avatar

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Avatar from '../Avatar';
@@ -15,17 +14,11 @@ const useStyles = makeStyles(theme => ({
     color: '#fff',
     padding: theme.spacing(4),
     textShadow: '2px 2px #848484'
-  },
-  paper: {
-    borderRadius: 25,
-    backgroundColor: '#ef5350',
-    marginBottom: theme.spacing(4),
-    padding: theme.spacing(4)
   }
 }));
 
 const Landing = props => {
-  const { user } = props;
+  const { signedIn, user } = props;
 
   const classes = useStyles();
 
@@ -57,9 +50,7 @@ const Landing = props => {
           justify="flex-start"
           alignItems="flex-end">
           <Grid item>
-            <Paper elevation={12} className={classes.paper}>
-              <Avatar user={user} />
-            </Paper>
+            <Avatar signedIn={signedIn} user={user} />
           </Grid>
         </Grid>
       </Grid>
