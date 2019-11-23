123855838ae1545e20a209c27ef1dade1a282eb5
petercolella <peter.joseph.colella@gmail.com>
2019-11-22 21:36:55 -0500

    adds nested list for questions in NavMenu (wip)

--- client/src/components/NavMenu/index.js
+++ client/src/components/NavMenu/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
+import Collapse from '@material-ui/core/Collapse';
+import Divider from '@material-ui/core/Divider';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import HelpIcon from '@material-ui/icons/Help';
 import HomeIcon from '@material-ui/icons/Home';
 import DashboardIcon from '@material-ui/icons/Dashboard';
+import { ReactComponent as Gift } from '../QuestionComponents/Anniversary/gift.svg';
+import { ReactComponent as Cake } from '../QuestionComponents/Birthday/cake.svg';
+import { ReactComponent as Reminder } from '../QuestionComponents/Nudges/reminder.svg';
+import { ReactComponent as Love } from '../QuestionComponents/Partner/love.svg';
+import { ReactComponent as Smartphone } from '../QuestionComponents/Phone/smartphone.svg';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -16,6 +26,16 @@ const useStyles = makeStyles(theme => ({
   link: {
     color: theme.palette.text.primary,
     textDecoration: 'none'
+  },
+  listItemText: {
+    marginLeft: '-1.5em'
+  },
+  nested: {
+    paddingLeft: theme.spacing(4)
+  },
+  svg: {
+    height: '1.25em',
+    width: '1.25em'
   }
 }));
 
@@ -24,31 +44,90 @@ const NavMenu = props => {
 
   const { setDrawerOpen } = props;
 
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
   return (
     <div className={classes.root}>
       <List component="nav" aria-label="main mailbox folders">
-        <ListItem button>
-          <ListItemIcon>
-            <HomeIcon />
-          </ListItemIcon>
-          <Link
-            to="/"
-            onClick={() => setDrawerOpen(false)}
-            className={classes.link}>
+        <Link
+          to="/"
+          onClick={() => setDrawerOpen(false)}
+          className={classes.link}>
+          <ListItem button>
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
             <ListItemText primary="Home" />
-          </Link>
-        </ListItem>
-        <ListItem button>
-          <ListItemIcon>
-            <DashboardIcon />
-          </ListItemIcon>
-          <Link
-            to="/dashboard"
-            onClick={() => setDrawerOpen(false)}
-            className={classes.link}>
+          </ListItem>
+        </Link>
+        <Link
+          to="/dashboard"
+          onClick={() => setDrawerOpen(false)}
+          className={classes.link}>
+          <ListItem button>
+            <ListItemIcon>
+              <DashboardIcon />
+            </ListItemIcon>
             <ListItemText primary="Dashboard" />
-          </Link>
+          </ListItem>
+        </Link>
+      </List>
+      <Divider />
+      <List>
+        <ListItem button onClick={handleClick}>
+          <ListItemIcon>
+            <HelpIcon />
+          </ListItemIcon>
+          <ListItemText primary="Questions" />
+          {open ? <ExpandLess /> : <ExpandMore />}
         </ListItem>
+        <Collapse in={open} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <ListItem button className={classes.nested}>
+              <ListItemIcon>
+                <Gift className={classes.svg} />
+              </ListItemIcon>
+              <ListItemText
+                className={classes.listItemText}
+                primary="Anniversary"
+              />
+            </ListItem>
+            <ListItem button className={classes.nested}>
+              <ListItemIcon>
+                <Cake className={classes.svg} />
+              </ListItemIcon>
+              <ListItemText
+                className={classes.listItemText}
+                primary="Birthday"
+              />
+            </ListItem>
+            <ListItem button className={classes.nested}>
+              <ListItemIcon>
+                <Reminder className={classes.svg} />
+              </ListItemIcon>
+              <ListItemText className={classes.listItemText} primary="Nudges" />
+            </ListItem>
+            <ListItem button className={classes.nested}>
+              <ListItemIcon>
+                <Love className={classes.svg} />
+              </ListItemIcon>
+              <ListItemText
+                className={classes.listItemText}
+                primary="Partner"
+              />
+            </ListItem>
+            <ListItem button className={classes.nested}>
+              <ListItemIcon>
+                <Smartphone className={classes.svg} />
+              </ListItemIcon>
+              <ListItemText className={classes.listItemText} primary="Phone" />
+            </ListItem>
+          </List>
+        </Collapse>
       </List>
     </div>
   );
