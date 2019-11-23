cfe0aac622dbbe052f5506dca818793b8b372ffc
petercolella <peter.joseph.colella@gmail.com>
2019-11-22 21:35:21 -0500

    adds Fade transition to Landing component

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import Fade from '@material-ui/core/Fade';
 import Grid from '@material-ui/core/Grid';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -26,38 +27,40 @@ const Landing = props => {
   const classes = useStyles();
 
   return (
-    <Container className={classes.container}>
-      <Grid
-        container
-        direction="column"
-        justify="space-between"
-        className={classes.container}>
+    <Fade in={true} timeout={1000}>
+      <Container className={classes.container}>
         <Grid
           container
-          direction="row"
-          justify="flex-end"
-          alignItems="flex-start">
-          <Grid item xs={12} sm={6}>
-            <Toolbar />
-            <Typography
-              variant="h2"
-              align="left"
-              className={classes.homeSaying}>
-              Planned Spontaneity
-            </Typography>
+          direction="column"
+          justify="space-between"
+          className={classes.container}>
+          <Grid
+            container
+            direction="row"
+            justify="flex-end"
+            alignItems="flex-start">
+            <Grid item xs={12} sm={6}>
+              <Toolbar />
+              <Typography
+                variant="h2"
+                align="left"
+                className={classes.homeSaying}>
+                Planned Spontaneity
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid
+            container
+            direction="row"
+            justify="flex-start"
+            alignItems="flex-end">
+            <Grid item>
+              <Avatar signedIn={signedIn} user={user} />
+            </Grid>
           </Grid>
         </Grid>
-        <Grid
-          container
-          direction="row"
-          justify="flex-start"
-          alignItems="flex-end">
-          <Grid item>
-            <Avatar signedIn={signedIn} user={user} />
-          </Grid>
-        </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </Fade>
   );
 };
 
