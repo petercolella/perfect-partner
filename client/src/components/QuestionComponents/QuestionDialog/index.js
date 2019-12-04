a3fca5eb696eff9e45ecbce9145dc5b56068beeb
petercolella <peter.joseph.colella@gmail.com>
2019-12-03 22:35:26 -0500

    updates QuestionDialog with refactoring/renaming

--- client/src/components/QuestionComponents/QuestionDialog/index.js
+++ client/src/components/QuestionComponents/QuestionDialog/index.js
@@ -1,17 +1,9 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import clsx from 'clsx';
-import CheckCircleIcon from '@material-ui/icons/CheckCircle';
-import ErrorIcon from '@material-ui/icons/Error';
-import InfoIcon from '@material-ui/icons/Info';
-import CloseIcon from '@material-ui/icons/Close';
-import { amber, green } from '@material-ui/core/colors';
-import IconButton from '@material-ui/core/IconButton';
-import Snackbar from '@material-ui/core/Snackbar';
-import SnackbarContent from '@material-ui/core/SnackbarContent';
-import WarningIcon from '@material-ui/icons/Warning';
 import { makeStyles } from '@material-ui/core/styles';
-import Slide from '@material-ui/core/Slide';
+
+import CloseIcon from '@material-ui/icons/Close';
+
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -20,29 +12,15 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Fade from '@material-ui/core/Fade';
 import Grow from '@material-ui/core/Grow';
+import IconButton from '@material-ui/core/IconButton';
+import Slide from '@material-ui/core/Slide';
+import Snackbar from '@material-ui/core/Snackbar';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 
-const variantIcon = {
-  success: CheckCircleIcon,
-  warning: WarningIcon,
-  error: ErrorIcon,
-  info: InfoIcon
-};
+import SnackbarContentWrapper from '../../SnackbarContentWrapper';
 
-const useStyles1 = makeStyles(theme => ({
-  success: {
-    backgroundColor: green[600]
-  },
-  error: {
-    backgroundColor: theme.palette.error.dark
-  },
-  info: {
-    backgroundColor: theme.palette.primary.main
-  },
-  warning: {
-    backgroundColor: amber[700]
-  },
+const useStyles = makeStyles(theme => ({
   click: {
     color: '#fff',
     padding: theme.spacing(4),
@@ -61,59 +39,18 @@ const useStyles1 = makeStyles(theme => ({
     height: '100vh',
     width: '100vw'
   },
-  icon: {
-    fontSize: 20
-  },
-  iconVariant: {
-    opacity: 0.9,
-    marginRight: theme.spacing(1)
-  },
   link: {
     color: theme.palette.text.primary,
     textDecoration: 'none'
-  },
-  message: {
-    display: 'flex',
-    alignItems: 'center'
   }
 }));
 
-const MySnackbarContentWrapper = React.forwardRef((props, ref) => {
-  const classes = useStyles1();
-  const { className, message, onClose, variant, ...other } = props;
-  const Icon = variantIcon[variant];
-
-  return (
-    <SnackbarContent
-      className={clsx(classes[variant])}
-      aria-describedby="client-snackbar"
-      message={
-        <span id="client-snackbar" className={classes.message}>
-          <Icon className={clsx(classes.icon, classes.iconVariant)} />
-          {message}
-        </span>
-      }
-      action={[
-        <IconButton
-          key="close"
-          aria-label="close"
-          color="inherit"
-          onClick={onClose}>
-          <CloseIcon className={classes.icon} />
-        </IconButton>
-      ]}
-      {...other}
-      ref={ref}
-    />
-  );
-});
-
-function TransitionUp(props) {
+const TransitionUp = props => {
   return <Slide {...props} direction="up" />;
-}
+};
 
 const QuestionDialog = props => {
-  const classes = useStyles1();
+  const classes = useStyles();
 
   const {
     cancel,
@@ -138,7 +75,6 @@ const QuestionDialog = props => {
   const [dialogOpen, setDialogOpen] = useState(false);
 
   const loadDialog = useCallback(() => {
-    setDialogOpen(false);
     setTimeout(() => {
       loadUserInfo();
       setDialogOpen(true);
@@ -149,24 +85,20 @@ const QuestionDialog = props => {
     loadDialog();
   }, [loadDialog, signedIn]);
 
-  function handleDialogClose(event, reason) {
+  const handleDialogClose = (event, reason) => {
     if (reason === 'clickaway' || reason === 'backdropClick') {
       return;
     }
 
     setDialogOpen(false);
-  }
+  };
 
-  function handleSnackbarClose(event, reason) {
+  const handleSnackbarClose = (event, reason) => {
     if (reason === 'clickaway') {
       return;
     }
 
     setSnackbarOpen(false);
-  }
-
-  const clickHandler = e => {
-    handleFormSubmit(e);
   };
 
   const reloadDialog = () => {
@@ -190,7 +122,7 @@ const QuestionDialog = props => {
     }
 
     return (
-      <MySnackbarContentWrapper
+      <SnackbarContentWrapper
         onClose={handleSnackbarClose}
         variant={variant}
         message={<span>{span}</span>}
@@ -267,7 +199,7 @@ const QuestionDialog = props => {
               <Button onClick={cancel} color="secondary">
                 Cancel
               </Button>
-              <Button onClick={clickHandler} color="primary">
+              <Button onClick={e => handleFormSubmit(e)} color="primary">
                 Submit
               </Button>
               <Link to={link} className={classes.link}>
