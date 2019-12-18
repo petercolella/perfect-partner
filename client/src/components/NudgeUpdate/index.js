fcee7afcf5267c4e458504371522adf7e6adf3a3
petercolella <peter.joseph.colella@gmail.com>
2019-12-17 23:45:27 -0500

    adds SnackbarContentWrapper to NudgeUpdate

--- client/src/components/NudgeUpdate/index.js
+++ client/src/components/NudgeUpdate/index.js
@@ -1,70 +1,19 @@
 import React from 'react';
-import clsx from 'clsx';
-import CheckCircleIcon from '@material-ui/icons/CheckCircle';
-import CloseIcon from '@material-ui/icons/Close';
-import { green } from '@material-ui/core/colors';
-import IconButton from '@material-ui/core/IconButton';
-import Snackbar from '@material-ui/core/Snackbar';
-import SnackbarContent from '@material-ui/core/SnackbarContent';
-import { makeStyles } from '@material-ui/core/styles';
-import Slide from '@material-ui/core/Slide';
+
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import MenuItem from '@material-ui/core/MenuItem';
+import Snackbar from '@material-ui/core/Snackbar';
+import Slide from '@material-ui/core/Slide';
+import TextField from '@material-ui/core/TextField';
+
+import SnackbarContentWrapper from '../SnackbarContentWrapper';
+
 import { ReactComponent as Pencil } from './pencil.svg';
-
-const useStyles1 = makeStyles(theme => ({
-  success: {
-    backgroundColor: green[600]
-  },
-  icon: {
-    fontSize: 20
-  },
-  iconVariant: {
-    opacity: 0.9,
-    marginRight: theme.spacing(1)
-  },
-  message: {
-    display: 'flex',
-    alignItems: 'center'
-  }
-}));
-
-const MySnackbarContentWrapper = React.forwardRef((props, ref) => {
-  const classes = useStyles1();
-  const { message, onClose, variant, ...other } = props;
-
-  return (
-    <SnackbarContent
-      className={clsx(classes[variant])}
-      aria-describedby="client-snackbar"
-      message={
-        <span id="client-snackbar" className={classes.message}>
-          <CheckCircleIcon
-            className={clsx(classes.icon, classes.iconVariant)}
-          />
-          {message}
-        </span>
-      }
-      action={[
-        <IconButton
-          key="close"
-          aria-label="close"
-          color="inherit"
-          onClick={onClose}>
-          <CloseIcon className={classes.icon} />
-        </IconButton>
-      ]}
-      {...other}
-      ref={ref}
-    />
-  );
-});
 
 function TransitionUp(props) {
   return <Slide {...props} direction="up" />;
@@ -88,28 +37,26 @@ const NudgeUpdate = props => {
 
   return (
     <div>
-      <div>
-        <Snackbar
-          anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'left'
-          }}
-          open={toastOpen}
-          autoHideDuration={2000}
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left'
+        }}
+        open={toastOpen}
+        autoHideDuration={2000}
+        onClose={handleToastClose}
+        TransitionComponent={TransitionUp}
+        ContentProps={{
+          'aria-describedby': 'message-id'
+        }}>
+        <SnackbarContentWrapper
           onClose={handleToastClose}
-          TransitionComponent={TransitionUp}
-          ContentProps={{
-            'aria-describedby': 'message-id'
-          }}>
-          <MySnackbarContentWrapper
-            onClose={handleToastClose}
-            variant="success"
-            message={
-              <span>{props.nudge.name} has been successfully updated.</span>
-            }
-          />
-        </Snackbar>
-      </div>
+          variant="success"
+          message={
+            <span>{props.nudge.name} has been successfully updated.</span>
+          }
+        />
+      </Snackbar>
       <Dialog
         fullWidth={true}
         open={props.dialogOpen}
