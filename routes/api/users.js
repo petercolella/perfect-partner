f8bc7da0b6e511f4a10fb88eb8dbc42a7d62ee2d
petercolella <peter.joseph.colella@gmail.com>
2019-12-25 02:55:42 -0500

    adds authware middleware to routes/api/users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,17 +1,18 @@
-const router = require("express").Router();
-const usersController = require("../../controllers/usersController");
+const router = require('express').Router();
+const usersController = require('../../controllers/usersController');
+const authware = require('../../middleware/authware');
 
 // Matches with "/api/users"
-router.route("/")
+router
+  .route('/')
   .get(usersController.findAll)
   .post(usersController.create);
 
 // Matches with "/api/users/:id"
 router
-  .route("/:id")
-  .get(usersController.findById)
+  .route('/:id')
+  .get(authware, usersController.findById)
   .put(usersController.update)
   .delete(usersController.remove);
-
 
 module.exports = router;
