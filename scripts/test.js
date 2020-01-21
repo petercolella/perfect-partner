bbae7194c8fb39416588666ba7ebfc1072ebc6ee
petercolella <peter.joseph.colella@gmail.com>
2020-01-20 22:34:48 -0500

    adds test.js to scripts folder

---
+++ scripts/test.js
@@ -0,0 +1,32 @@
+const { DateTime } = require('luxon');
+const mongoose = require('mongoose');
+
+const db = require('../models');
+const textController = require('../controllers/textController');
+
+const now = DateTime.local();
+const nowDayOfYear = now.toFormat('o');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pp', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+mongoose.set('useCreateIndex', true);
+
+console.log(`
+now.ts: ${now.ts}
+now.loc.locale: ${now.loc.locale}
+now.loc.intl: ${now.loc.intl}
+now.o: ${now.o}`);
+console.log('now.c:', now.c);
+console.log('nowDayOfYear:', nowDayOfYear);
+
+async function runMethods() {
+  await textController.runAnniversaryNudges();
+  await textController.runBirthdayNudges();
+
+  process.exit(0);
+}
+
+runMethods();
