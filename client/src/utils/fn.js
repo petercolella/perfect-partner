01c0ca4e4bd5a386ab1e1e0bceb8b9f510cf8d4f
petercolella <peter.joseph.colella@gmail.com>
2020-01-31 22:37:02 -0500

    adds localToUTC & UTCToLocal methods to fn.js

--- client/src/utils/fn.js
+++ client/src/utils/fn.js
@@ -1,4 +1,7 @@
 export default {
+  capitalizeFirstLetter: function(str) {
+    return `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
+  },
   formatPhoneNumber: function(num) {
     const first3 = num.slice(0, 3);
     const second3 = num.slice(3, 6);
@@ -6,7 +9,17 @@ export default {
 
     return `(${first3}) ${second3}-${last4}`;
   },
-  capitalizeFirstLetter: function(str) {
-    return `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
+  localToUTC: function(date) {
+    console.log('localToUTC:', date, date.toISO());
+    const newDate = date.setZone('UTC').set({ hour: 0 });
+    console.log(newDate, newDate.toISO());
+    return newDate;
+  },
+  UTCToLocal: function(date) {
+    console.log('UTCToLocal:', date, date.toISO());
+    const newHour = date.hour - date.offset / 60;
+    const newDate = date.set({ hour: newHour });
+    console.log(newHour, newDate, newDate.toISO());
+    return newDate;
   }
 };
