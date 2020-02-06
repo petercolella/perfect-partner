45cee0ca0e3b96363cb95bb51d343361ee4790ed
petercolella <peter.joseph.colella@gmail.com>
2020-02-06 00:31:05 -0500

    removes console logs from TZ functions in fn.js

--- client/src/utils/fn.js
+++ client/src/utils/fn.js
@@ -10,16 +10,12 @@ export default {
     return `(${first3}) ${second3}-${last4}`;
   },
   localToUTC: function(date) {
-    console.log('localToUTC:', date, date.toISO());
     const newDate = date.setZone('UTC').set({ hour: 0 });
-    console.log(newDate, newDate.toISO());
     return newDate;
   },
   UTCToLocal: function(date) {
-    console.log('UTCToLocal:', date, date.toISO());
     const newHour = date.hour - date.offset / 60;
     const newDate = date.set({ hour: newHour });
-    console.log(newHour, newDate, newDate.toISO());
     return newDate;
   }
 };
