1f246f9e0e850006e371c836bdecd5e3b9eb4464
petercolella <peter.joseph.colella@gmail.com>
2020-02-14 22:35:58 -0500

    updates Phone with QuestionPage component

--- client/src/components/QuestionComponents/Phone/index.js
+++ client/src/components/QuestionComponents/Phone/index.js
@@ -1,9 +1,10 @@
-import React, { useState, useCallback, useEffect } from 'react';
-import QuestionDialog from '../QuestionDialog';
-import API from '../../../utils/API';
+import React from 'react';
 import { ReactComponent as Smartphone } from './smartphone.svg';
 
-const state = {
+import QuestionPage from '../QuestionPage';
+
+const question = {
+  key: 'phone',
   label: 'Phone',
   nextQuestionLink: '/dashboard',
   placeholder: 'Enter with no dashes or spaces.',
@@ -12,80 +13,15 @@ const state = {
 };
 
 const Phone = props => {
-  const id = sessionStorage.getItem('currentUserId');
   const { loadUserInfo, signedIn, user } = props;
-  const [phone, setPhone] = useState('');
-  const [res, setRes] = useState(null);
-  const [snackbarOpen, setSnackbarOpen] = useState(false);
-  const [variant, setVariant] = useState(null);
-
-  const loadPhone = useCallback(() => {
-    if (id) {
-      API.getUser(id).then(res => {
-        if (res.data.phone) {
-          setPhone(res.data.phone);
-        }
-      });
-    }
-  }, [id]);
-
-  useEffect(() => {
-    loadPhone();
-  }, [loadPhone]);
-
-  const handleSnackbarOpen = variant => {
-    setVariant(variant);
-    setSnackbarOpen(true);
-  };
-
-  const handleFormSubmit = () => {
-    const phoneRegEx = phone.replace(/\D/g, '');
-
-    if (phoneRegEx.length !== 10) {
-      handleSnackbarOpen('warning');
-      return;
-    }
-
-    API.updateUser(user._id, {
-      phone: phoneRegEx
-    })
-      .then(res => {
-        loadUserInfo();
-        setRes(res.data.phone);
-        handleSnackbarOpen('success');
-      })
-      .catch(err => {
-        // captures error message after last colon and space
-        const [errMsg] = err.response.data.match(/(?! )[^:]+$/);
-        setRes(errMsg);
-        handleSnackbarOpen('error');
-      });
-  };
-
-  const handleInputChange = event => {
-    const { value } = event.target;
-    setPhone(value);
-  };
 
   return (
-    <QuestionDialog
-      cancel={loadPhone}
-      firstName={user.firstName}
-      handleFormSubmit={handleFormSubmit}
-      handleInputChange={handleInputChange}
+    <QuestionPage
       Image={Smartphone}
-      label={state.label}
-      link={state.nextQuestionLink}
       loadUserInfo={loadUserInfo}
-      placeholder={state.placeholder}
-      question={state.question}
-      res={res}
+      question={question}
       signedIn={signedIn}
-      snackbarOpen={snackbarOpen}
-      setSnackbarOpen={setSnackbarOpen}
-      title={state.title}
-      userField={phone}
-      variant={variant}
+      user={user}
     />
   );
 };
