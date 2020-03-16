28e6d74faf7a0f4dc5ead5fd3da8dc3a12510bc6
petercolella <peter.joseph.colella@gmail.com>
2020-03-15 23:39:42 -0400

    adds authware middleware to /api/dates post route

--- routes/api/dates.js
+++ routes/api/dates.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const customDatesController = require('../../controllers/customDatesController');
+const authware = require('../../middleware/authware');
 
 // Matches with "/api/nudges"
-router.route('/').post(customDatesController.create);
+router.route('/').post(authware, customDatesController.create);
 
 // Matches with "/api/nudges/:id"
 router
