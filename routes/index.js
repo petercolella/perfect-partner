d406085287385f28e26a703315b91552eac6b1b3
petercolella <peter.joseph.colella@gmail.com>
2020-05-02 13:12:45 -0400

    updates routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -5,16 +5,8 @@ const apiRoutes = require('./api');
 // API Routes
 router.use('/api', apiRoutes);
 
-// If no API routes are hit, send the React app
-router.use((req, res) => {
-  res.sendFile(
-    path.join(
-      __dirname,
-      process.env.NODE_ENV === 'production'
-        ? '../client/build/index.html'
-        : '../client/public/index.html'
-    )
-  );
-});
+router.use((req, res) =>
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+);
 
 module.exports = router;
