947ab13c0d35e3a9588a41d031a9116fbff82150
petercolella <peter.joseph.colella@gmail.com>
2020-07-28 20:50:23 -0400

    updates index.js with UserProvider and ContextWrapper

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider as UserProvider } from './context/UserContext';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const ContextWrapper = () => {
+  return (
+    <UserProvider>
+      <App />
+    </UserProvider>
+  );
+};
+
+ReactDOM.render(<ContextWrapper />, document.getElementById('root'));
