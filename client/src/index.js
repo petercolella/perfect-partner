bae153ff02a065cf6b71f105b20951716da0a2fc
petercolella <peter.joseph.colella@gmail.com>
2020-07-29 20:43:13 -0400

    adds SnackbarProvider to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider as UserProvider } from './context/UserContext';
+import { Provider as SnackbarProvider } from './context/SnackbarContext';
 import App from './App';
 
 const ContextWrapper = () => {
   return (
     <UserProvider>
-      <App />
+      <SnackbarProvider>
+        <App />
+      </SnackbarProvider>
     </UserProvider>
   );
 };
