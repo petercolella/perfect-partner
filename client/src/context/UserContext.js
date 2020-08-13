d685ed0f0ef9776dc9ca85b6a99acea00d28c5c8
petercolella <peter.joseph.colella@gmail.com>
2020-08-12 21:27:05 -0400

    updates UserContext with onFailure, onSuccess, setErrorMessage, setSignedIn, and signOut dispatch methods;

--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -1,12 +1,32 @@
 import createDataContext from './createStateContext';
 import API from '../utils/API';
 
+const noUser = {
+  anniversaryDate: '',
+  anniversaryReminders: [],
+  birthDate: '',
+  birthdayReminders: [],
+  email: '',
+  firstName: '',
+  imageUrl:
+    'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png',
+  lastName: '',
+  name: '',
+  nudges: [],
+  partnerName: '',
+  phone: ''
+};
+
 const userReducer = (state, action) => {
   switch (action.type) {
     case 'set_current_user':
       return { ...state, user: action.payload };
     case 'set_error_message':
       return { ...state, errorMessage: action.payload };
+    case 'setSignedIn':
+      return { ...state, signedIn: action.payload };
+    case 'set_user_error':
+      return { user: noUser, errorMessage: action.payload, signedIn: false };
     default:
       return state;
   }
@@ -15,6 +35,36 @@ const userReducer = (state, action) => {
 const loadCurrentUser = dispatch => res => {
   if (res) {
     dispatch({ type: 'set_current_user', payload: res.data });
+  }
+};
+
+const onFailure = dispatch => err => {
+  const errStr = err.response ? err.response.data.split(', ')[0] : err;
+  dispatch({ type: 'set_user_error', payload: errStr });
+};
+
+const onSuccess = dispatch => async googleUser => {
+  console.log('Signed in as: ' + googleUser.getBasicProfile().getName());
+  const id_token = googleUser.getAuthResponse().id_token;
+  sessionStorage.setItem('id_token', id_token);
+
+  try {
+    const id = await API.tokenSignInAxios(id_token);
+    if (id) {
+      sessionStorage.setItem('currentUserId', id);
+      API.getUser(id)
+        .then(res => {
+          dispatch({ type: 'set_current_user', payload: res.data });
+        })
+        .catch(err => {
+          const errStr = err.response ? err.response.data.split(', ')[0] : err;
+          dispatch({ type: 'set_user_error', payload: errStr });
+          sessionStorage.removeItem('currentUserId');
+          sessionStorage.removeItem('id_token');
+        });
+    }
+  } catch (err) {
+    console.log(err.response.data);
   }
 };
 
@@ -27,14 +77,45 @@ const reloadCurrentUser = dispatch => () => {
       })
       .catch(err => {
         const errStr = err.response ? err.response.data.split(', ')[0] : err;
-        dispatch({ type: 'set_error_message', payload: errStr });
+        dispatch({ type: 'set_user_error', payload: errStr });
       });
   }
 };
 
+const setErrorMessage = dispatch => message => {
+  dispatch({ type: 'set_error_message', payload: message });
+};
+
+const setSignedIn = dispatch => signedIn => {
+  dispatch({ type: 'setSignedIn', payload: signedIn });
+};
+
+const signOut = dispatch => () => {
+  if (window.gapi && window.gapi.auth2) {
+    const GoogleAuth = window.gapi.auth2.getAuthInstance();
+    GoogleAuth.signOut()
+      .then(() => {
+        console.log('User signed out.');
+      })
+      .catch(err => console.log(err));
+  }
+
+  dispatch({ type: 'set_current_user', payload: noUser });
+  sessionStorage.removeItem('currentUserId');
+  sessionStorage.removeItem('id_token');
+};
+
 export const { Provider, Context } = createDataContext(
   userReducer,
-  { loadCurrentUser, reloadCurrentUser },
+  {
+    loadCurrentUser,
+    onFailure,
+    onSuccess,
+    reloadCurrentUser,
+    setErrorMessage,
+    setSignedIn,
+    signOut
+  },
   {
     user: {
       anniversaryDate: '',
@@ -51,6 +132,7 @@ export const { Provider, Context } = createDataContext(
       partnerName: '',
       phone: ''
     },
-    errorMessage: ''
+    errorMessage: '',
+    signedIn: false
   }
 );
