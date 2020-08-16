b9ee44db33e6cd9c582851c83906ebfffbe143fa
petercolella <peter.joseph.colella@gmail.com>
2020-08-16 13:41:18 -0400

    updates formatPhoneNumber method to include low line and hair space unicode characters

--- client/src/utils/fn.js
+++ client/src/utils/fn.js
@@ -3,11 +3,23 @@ export default {
     return `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
   },
   formatPhoneNumber: num => {
-    const first3 = num.slice(0, 3);
-    const second3 = num.slice(3, 6);
-    const last4 = num.slice(6);
+    if (num.length) {
+      let chars = '';
+      for (let i = 0; i < 10; i++) {
+        if (!num[i]) {
+          chars += '\u005F';
+        } else {
+          chars += num[i];
+        }
+      }
+      const first3 = chars.substring(0, 3).split('').join('\u200A');
+      const second3 = chars.substring(3, 6).split('').join('\u200A');
+      const last4 = chars.substring(6).split('').join('\u200A');
 
-    return `(${first3}) ${second3}-${last4}`;
+      return `(${first3}) ${second3} - ${last4}`;
+    }
+
+    return num;
   },
   localToUTC: date => {
     const newDate = date.setZone('UTC').set({ hour: 0 });
