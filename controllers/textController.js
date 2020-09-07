b5a969a4ac1c3bf8c7dba46d62b527f96baba31a
petercolella <peter.joseph.colella@gmail.com>
2020-09-07 18:27:43 -0400

    removes unused code from textController

--- controllers/textController.js
+++ controllers/textController.js
@@ -9,13 +9,6 @@ const {
 } = require('../scripts/fn');
 const { DateTime } = require('luxon');
 
-// Download the helper library from https://www.twilio.com/docs/node/install
-// Your Account Sid and Auth Token from twilio.com/console
-// DANGER! This is insecure. See http://twil.io/secure
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken);
-
 const getNowData = () => {
   const now = DateTime.utc();
   console.log('now:', now.toISO());
